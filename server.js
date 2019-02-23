f0ef7ae8885469cd59e3c38d8127f9959b5ab984
Anthony Maddatu <amaddatu@gmail.com>
2019-02-23 14:12:03 -0500

    Add mysql

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require("express");
 var exphbs = require("express-handlebars");
+var mysql = require("mysql");
 
 // Create an instance of the express app.
 var app = express();
@@ -8,6 +9,13 @@ var app = express();
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
+
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect( (err)=>{
+  if(err) console.log( err );
+  console.log(connection.threadId);
+});
 
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -38,6 +46,10 @@ app.get("/lunches", function(req, res) {
     eater: "david"
   });
 });
+
+app.get("/connection-id", function(req, res){
+  res.send("connected as " + connection.threadId);
+})
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
